2128f29614270827dca7e0c978855109cb0e6149
hkamatsuka <hiroshi_k2k@nifty.com>
2015-11-11 23:25:25 +0800

    add

---
+++ IOT/infrastructure/batch/InitSensor_01_02.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+pid=/run/sensor01_02.pid
+dir=/home/pi/shell/Python/
+script=FileCreate_01_02.py
+
+case "$1" in
+  start|"")
+	python ${dir}${script} &
+        ps -ef | grep ${script} | grep -v grep | awk '{print $2}' > ${pid}
+	;;
+  restart)
+        kill -9 `cat ${pid}`
+        sleep 1
+	python ${dir}${script} &
+        ps -ef | grep ${script} | grep -v grep | awk '{print $2}' > ${pid}
+	;;
+  stop)
+        kill -9 `cat ${pid}`
+	;;
+esac
